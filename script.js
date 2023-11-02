46885a1562f213df22bf5c6d398ee1a74146e49a
Jaiten <Jaitensahu9@gmail.com>
2023-11-02 12:06:50 +0530

    update

--- script.js
+++ script.js
@@ -125,25 +125,50 @@ function appendMainData(result, movie) {
     createEleAppendToMain(movieArray);
   }
 }
+let allItems=document.querySelectorAll(".showMovieItem");
+
 function createEleAppendToMain(movieArray) {
   movieArray.forEach((ele, idx) => {
     if (idx < 20) {
+      // console.log(ele);
       let div1 = document.createElement("div");
       let poster = ele.poster_path;
       let title = ele.original_title;
       div1.classList.add("showMovieItem");
       div1.innerHTML = `
               <img src=https://www.themoviedb.org/t/p/w220_and_h330_face${poster} alt="">
-              <p class="main-title">${title}</p>
+              <p class="main-title">
+              <span >${title}</span>
+              <span>,${ele.Year}</span>
+              </p>
               `;
       showMovie.appendChild(div1);
+      allMovieCard=document.querySelectorAll(".showMovieItem")
+      addEventonBtn( allMovieCard)
+      // showMovieDetails(allItems);
+
     }
   });
 }
 
-fecthDataAgain("popular", 1);
+// function showMovieDetails(allItems){
+//   allItems.forEach(ele=>{
+//     ele.addEventListener("click", showDetailCallBack)
+//   })
+// }
+// async function showDetailCallBack(e){
+// let t=(e.target.parentElement.children[1].innerText);
+// t=t.split(" ").join("+");
+// let data= await fetch(`http://www.omdbapi.com/?apikey=340ca97e&t=${t}&plot=full`);
+// let res= await data.json();
+// console.log(res)
 
+// }
+
+fecthDataAgain("popular", 1);
+// Deboucing concept
 let searchBtn = document.querySelector(".searchBtn");
+
 function betterFunc(callFunc, delay){
     let timer;
     return ()=>{
@@ -158,56 +183,122 @@ function betterFunc(callFunc, delay){
 
 searchBtn.addEventListener("input", (e) => {
   e.preventDefault()
+  showMovie.innerHTML="";
+  loader.style.display="flex"
      myDebounce();
+     
     });
 
-let myDebounce=betterFunc(getData, 2000);
+let myDebounce=betterFunc(getData, 500);
 
 async function getData() {
+  loader.style.display="none"
   let val = searchInput.value.split(" ").join("+");
-  let data = await fetch(
-    `http://www.omdbapi.com/?&apikey=340ca97e&s=${searchInput.value}&page=1`
-  );
-  let data2=await fetch("http://www.omdbapi.com/?&apikey=340ca97e&id=tt8110330")
-  let res2= await data2.json();
-  console.log(res2);
-  let res = await data.json();
-  console.log(res);
-  showMovie.innerText = "";
-  res.Search.forEach((ele) => {
-    let div1 = document.createElement("div");
-    div1.classList.add("showMovieItem");
-    div1.innerHTML = `
-              <img src=${ele.Poster} alt="">
-              <p class="main-title">
-              <span >${ele.Title}</span>
-              <span>,${ele.Year}</span></p>
-              `;
-    showMovie.appendChild(div1);
+  try {
+    let data = await fetch(
+      `http://www.omdbapi.com/?&apikey=340ca97e&s=${searchInput.value}&page=1`
+    );
+    let data2=await fetch("http://www.omdbapi.com/?&apikey=340ca97e&id=tt8110330")
+    let res2= await data2.json();
+    // console.log(res2);
+    let res = await data.json();
+    console.log(res);
+    showMovie.innerText = "";
+    res.Search.forEach((ele) => {
+      let div1 = document.createElement("div");
+      div1.classList.add("showMovieItem");
+      div1.innerHTML = `
+                <img src=${ele.Poster} alt="">
+                <p class="main-title">
+                <span >${ele.Title}</span>
+                <span>,${ele.Year}</span></p>
+                `;
+      showMovie.appendChild(div1);
+      
+      
+    });
+    allMovieCard=document.querySelectorAll(".showMovieItem")
+      addEventonBtn(allMovieCard);
+  } catch (error) {
+    if(searchInput.value==""){
+      showMovie.innerHTML="<h1>Please Enter Movie Name</h1>"
+    }else{
+      showMovie.innerHTML="<h1>Movie Not Found!!!</h1>"
+    }
     
-    
-  });
-  allMovieCard=document.querySelectorAll(".showMovieItem")
-    addEventonBtn();
+  }
+
 }
+
+
 
 let allMovieCard;
 
-function addEventonBtn(){
-  allMovieCard=document.querySelectorAll(".showMovieItem")
+function addEventonBtn(allMovieCard){
+  // allMovieCard=document.querySelectorAll(".showMovieItem")
   allMovieCard.forEach(ele=>{
     ele.addEventListener("click",movieDetail)
   })
 }
 
-
+let showDetailPopUp=document.querySelector(".background");
 async function movieDetail(e){
   let movieClicked= e.target.parentElement.children[1].children[0].innerText;
-  console.log(movieClicked);
-  let data= await fetch(`http://www.omdbapi.com/?apikey=340ca97e&t=dil&plot=full`);
+  // console.log(movieClicked);
+  let val=movieClicked.split(" ").join("+");
+  let data= await fetch(`http://www.omdbapi.com/?apikey=340ca97e&t=${val}&plot=full`);
   let res= await data.json();
-  console.log(res);
+  let div1=document.createElement("div");
+  div1.classList.add("showMovieDetails");
+  div1.innerHTML=
+  `
+  <i class="fa-solid fa-xmark"></i>
+  <div class="imgD">
+      <img src=${res.Poster} alt="">
+  </div>
+  <div class="detailsRight">
+      <div class="detailsRight-sub">
+          <!-- <div class="detailsRight-heading"> -->
+          <p><span>${res.Title}</span>
+              <spna>${res.Year}</spna>
+          </p>
+          <p>
+              <span class="rated">${res.Rated}</span>
+              <span class="date">10/20/2023</span>.
+              <span>${res.Genre}</span>
+              <span class="hr">${res.Runtime}</span>
+          </p>
+          <div class="icons">
+              <i class="fa-solid fa-list"></i>
+              <i class="fa-solid fa-heart"></i>
+              <i class="fa-solid fa-bookmark"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-download"></i>
+              <i class="fa-regular fa-circle-play"></i>
+          </div>
+          <h3>Overview</h3>
+          <p class="description">${res.Plot}</p>
+          <div class="directors">
+              <p>${res.Director}<br><span>Director</span></p>
+              <p>${res.Writer} <br><span>Writer</span></p>
+          </div>
+          <!-- </div> -->
+      </div>
+  </div>
+  
+  `
+  
+  // console.log("hh");
+  showDetailPopUp.replaceChildren(div1);
+  showDetailPopUp.style.display="flex"
+  document.querySelector(".showMovieDetails").classList.add("animate__zoomIn")
+  // showDetailPopUp.classList.add("animate__lightSpeedInRight")
+  closePopbtn=document.querySelector(".fa-xmark")
+  closePopUp(closePopbtn)
+}
 
-  
-  
+function closePopUp(closePopbtn){
+  closePopbtn.addEventListener("click",()=>{
+    showDetailPopUp.style.display="none"
+  })
 }
